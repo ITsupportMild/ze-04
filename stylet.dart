7732da8a26a348dbe72d1171a02c61fc05bb1e5f
ITsupportMild <154951827+ITsupportMild@users.noreply.github.com>
2024-01-17 19:30:42 +0700

    Add files via upload

---
+++ stylet.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Stylet extends StatelessWidget {
+  Stylet({super.key});
+  Widget build(context) {
+    return Text(
+      "สวัสดีท่านผู้เจริญ",
+      style: TextStyle(fontSize: 170, color: Colors.white),
+    );
+  }
+}
